3f7ab6eb78c92e5e7b72fcaabbfeadf93fc6cf37
lipesc <felipedrhouse@hotmail.com>
2025-06-29 17:59:57 -0300

    my changes from local on the lessons

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,19 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>favorite Movie: deadstream</li>
+      <li>music: deuces</li>
+      <li>book: hellraiser</li>
+    </ol>
+    <router-outlet></router-outlet>
+
   `,
-  styles: ``,
+  styles: `
+  ol {
+   list-style-type: upper-roman;
+
+  }
+`
 })
 export class AppComponent {}
