2e0e67484cb4e2a3da33a3855e8e1dbc940d78e6
lipesc <felipedrhouse@hotmail.com>
2025-06-30 14:52:42 -0300

    lesson 5 done

--- projects/05-control-flow-for/src/app/app.component.ts
+++ projects/05-control-flow-for/src/app/app.component.ts
@@ -6,31 +6,34 @@ import { Component } from '@angular/core';
   template: `
     <section class="container">
       <!-- This article element represents and entire listing -->
-      <article class="listing">
-        <div class="image-parent">
-          <img class="product-image" src="https://placehold.co/100x100" />
-        </div>
-        <section class="details">
-          <p class="title"><!-- car make and model--></p>
-          <hr />
-          <p class="detail">
-            <span>Year</span>
-            <span><!-- year --></span>
-          </p>
-          <div class="detail">
-            <span>Transmission</span>
-            <span><!-- transmission --></span>
-          </div>
-          <p class="detail">
-            <span>Mileage</span>
-            <span><!-- miles --></span>
-          </p>
-          <p class="detail">
-            <span>Price</span>
-            <span><!-- price --></span>
-          </p>
-        </section>
-      </article>
+       @for (item of carList; track item) {
+            
+         <article class="listing">
+           <div class="image-parent">
+             <img class="product-image" src="https://placehold.co/100x100" />
+           </div>
+           <section class="details">
+             <p class="title">{{item.make}} {{item.model}}</p>
+             <hr />
+             <p class="detail">
+               <span>Year</span>
+               <span><!-- year -->{{item.year}}</span>
+             </p>
+             <div class="detail">
+               <span>Transmission</span>
+               <span>{{item.transmission}}</span>
+             </div>
+             <p class="detail">
+               <span>Mileage</span>
+               <span>{{item.miles}}</span>
+             </p>
+             <p class="detail">
+               <span>Price</span>
+               <span>{{item.price}}</span>
+             </p>
+           </section>
+         </article>
+          }
     </section>
   `,
   styleUrl: 'app.component.css',
